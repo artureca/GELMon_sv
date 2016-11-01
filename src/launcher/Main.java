2125e3a810c17eb324e2789418f08c26c186bd7a
artureca <artureca@artureca-pc>
2016-11-01 19:50:58 +0000

    Merge origin/master
    Conflicts:
    	src/sdr/com/Proto_PI.java

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FEUP / MIEEC / SETEC / 2016 / Group B
+ * http://fe.up.pt/
+ *
+ * 201202877 / Artur Antunes
+ * 200907504 / Bruno Gonçalves
+ * 201106784 / Eugenio Carvalhido
+ * 201105402 / Fábio Cunha
+ * 201206114 / Filipe Rocha
+ * 201105621 / José Carvalho
+ * 201100603 / Luís Pinto
+ * 201200617 / Pedro Fonseca
+ * 201201704 / Raquel Ribeiro
+ * 201202703 / Rubens Figueiredo
+ * 201109265 / Vânia Vieira
  */
+
 package launcher;
 
 import analysis.com.Proto_AZGO;
