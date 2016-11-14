ae9ec6c613304ee64f06aab7fe6502e0b9cf8603
Bruno <Bruno@Bruno-TOSH>
2016-11-14 21:18:26 +0000

    metodo set image

---
+++ src/analysis/db/Image.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analysis.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Image {
+    
+    
+    
+    public Image() {
+    }
+    
+    PreparedStatement st1;
+    
+    public boolean setImage(Connection conn,String path,Timestamp i_time,Timestamp f_time){
+        
+        try{
+            String query= "INSERT INTO images (path,i_time,f_time) values(?,?,?) ";
+            st1 = conn.prepareStatement(query);
+            st1.setString(1,path);
+            st1.setTimestamp(2,i_time);
+            st1.setTimestamp(3,f_time);
+            st1.executeUpdate();
+         
+        }catch(Exception ex){
+            System.out.println("setImage error:"+ex);
+        }
+        
+     return true;   
+    }
+    
+    
+    
+}
